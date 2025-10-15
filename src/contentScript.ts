9f141c30f535c7edf41efdfdfefa66f63dd1c3c0
awtnb <arikaebanataw@gmail.com>
2025-10-15 15:40:01 +0900

    fix

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -107,9 +107,8 @@ const getHassoIraishoLine = (): string => {
 
 const getContentDetail = (): string => {
   return (
-    document
-      .getElementById('cont_box_m30')
-      ?.textContent.replace(/，/g, '、')
+    (document.getElementById('cont_box_m30')?.textContent || '')
+      .replace(/，/g, '、')
       .trim() || ''
   );
 };
